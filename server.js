be694ae7be19808b06bf5c0b26412375e6a29471
Heaki1 <115172360+Heaki1@users.noreply.github.com>
2025-06-17 13:54:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const cors = require('cors');
-app.use(cors());
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
+
+const app = express();      
+app.use(cors());   
 
 // 🔒 Replace these with your actual osu! API credentials
 const client_id = '41700';
